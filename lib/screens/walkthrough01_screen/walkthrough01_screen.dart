91965520fcef1a107fa7b4cf79679dbecd10d096
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-11-09 08:51:14 +0330

    Create walkthrough 1 screen.

---
+++ lib/screens/walkthrough01_screen/walkthrough01_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class WalkthroughOneScreen extends StatelessWidget {
+  const WalkthroughOneScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/images/planeLogo.png'),
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 25.0),
+                Text(
+                  'Book Cheapest Flight',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text(
+                  'We compare prices from 200+ booking\nsite to help you find the lowest price ',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.05,
+            right: MediaQuery.of(context).size.width * 0.05,
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 5.0,
+                  ),
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.91,
+            right: MediaQuery.of(context).size.width * 0.05,
+            child: GestureDetector(
+              onTap: () {},
+              child: Text(
+                'Next',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
